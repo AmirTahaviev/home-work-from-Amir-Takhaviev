50868dd5015dd90b4e2005a497574fceb6019a92
AmirTahaviev <amir.tahaviev@bk.ru>
2025-09-25 23:40:31 -0700

    .

---
+++ Семинар 2/упражнение 6.py
@@ -0,0 +1,9 @@
+n = list(map(int, input().split()))
+
+for i in range(len(n)):
+    c = 0
+    for j in range(len(n)):
+        if n[i] == n[j]:
+            c += 1
+    if c == 1:
+        print(n[i])
