d00191a3813178f4f25b1e4c44424c426ea4ddd6
AmirTahaviev <amir.tahaviev@bk.ru>
2025-10-03 01:57:21 -0700

    .

---
+++ Семинар 3/упражнение 4.py
@@ -0,0 +1,7 @@
+def f(size, symb):
+    for i in range(1, (size+1)//2):
+        print(symb*i)
+    for i in range((size+1)//2, 0, -1):
+        print(symb*i)
+s = input().split()
+f(int(s[0]), s[1])
