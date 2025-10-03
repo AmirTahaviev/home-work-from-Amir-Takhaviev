1c8ffd295ab066287223d6b4ac9e307ac25efa2d
AmirTahaviev <amir.tahaviev@bk.ru>
2025-10-03 02:01:34 -0700

    .

---
+++ Семинар 3/упражнение 6.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.figure(figsize = (10, 8))
+x = [1.01, 2.59, 3.03, 5.4, 7.33]
+y = [0.41, 0.84, 1.11, 3.22, 5.00]
+plt.scatter(x, y)
+plt.errorbar(x, y, yerr = 0.2, xerr= 0.1, color = 'k',  linestyle = 'None')
+
+mnk = np.polyfit(x,y,1)
+z = np.poly1d(mnk)
+print(mnk)
+print(z)
+
