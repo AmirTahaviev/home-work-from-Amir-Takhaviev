3c935494ced24f074b3c1f2ee1f603420211218e
AmirTahaviev <amir.tahaviev@bk.ru>
2025-10-16 18:58:11 +0300

    .

---
+++ Семинар 4/упражнение 8.py
@@ -0,0 +1,8 @@
+X = list(map(int, input().split()))
+Y = list(map(int, input().split()))
+A = set(X)
+B = set(Y)
+
+print('Уникальные элементы х:', A - B)
+print('Уникальные элементы y:', B - A)
+print('Содержатся в обоих списках:', A & B)
